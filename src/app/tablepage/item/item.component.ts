79a0ddfb9ba4a3833218554649944cbf45fb78f9
Роман Неред <romaus@mail.ru>
2021-07-20 00:54:58 +0300

    add: "show item field by edit url"

--- src/app/tablepage/item/item.component.ts
+++ src/app/tablepage/item/item.component.ts
@@ -1,9 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Product} from '../../types/product';
 import {LocalStorageService} from '../../services/localstorageservice';
-import {ActivatedRoute, ActivatedRouteSnapshot} from '@angular/router';
-import {switchMap} from 'rxjs/operators';
+import {ActivatedRoute} from '@angular/router';
 import {Subscription} from 'rxjs';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { ValidatorsDirective } from '../../shared/Customvalidators.directive';
+import {DateFormatService} from '../../services/dateformatservice';
 
 @Component({
   selector: 'app-item',
@@ -16,10 +18,13 @@ export class ItemComponent implements OnInit {
   product: Product = {};
   id = '';
   stream$: Subscription;
+  itemForm: any;
 
   constructor(
     private localStorageService: LocalStorageService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private customValidator: ValidatorsDirective,
+    public formatdate: DateFormatService
   ) {
     this.stream$ = route.params.subscribe(params => {
       this.id = params.id;
@@ -30,8 +35,36 @@ export class ItemComponent implements OnInit {
     this.localStorageService.getItemsID('items', this.id)
       .subscribe(item => {
         this.product = item;
-        console.log(this.product)
+        this.product.startdate = this.formatdate.convertByMomentToUS(this.product.startdate);
+        this.product.enddate = this.formatdate.convertByMomentToUS(this.product.enddate);
+        console.log(this.product);
       });
-  }
 
+    this.itemForm = new FormGroup({
+      name: new FormControl(this.product.name, [Validators.required]),
+      email: new FormControl(this.product.email, [
+        Validators.required,
+        Validators.email
+      ]),
+      phone: new FormControl(this.product.phone, [
+        Validators.required,
+        this.customValidator.phoneCodeBelarusValidator()
+      ]),
+      description: new FormControl(this.product.description, [
+        Validators.required,
+        Validators.minLength(20)
+      ]),
+      category: new FormControl(this.product.category),
+      price: new FormControl(this.product.price),
+      quantity: new FormControl(this.product.quantity),
+      startdate: new FormControl(this.product.startdate, [
+        Validators.required,
+        this.customValidator.dateValidator()
+      ]),
+      enddate: new FormControl(this.product.enddate, [
+        Validators.required,
+        this.customValidator.dateValidator()
+      ])
+    });
+  }
 }
--- src/app/tablepage/item/item.module.ts
+++ src/app/tablepage/item/item.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { ItemComponent } from './item.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {InputMaskModule} from 'primeng/inputmask';
+import {InputNumberModule} from 'primeng/inputnumber';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {InputTextModule} from 'primeng/inputtext';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {ButtonModule} from 'primeng/button';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
-    imports: [],
+  imports: [
+    ReactiveFormsModule,
+    InputMaskModule,
+    InputNumberModule,
+    RadioButtonModule,
+    InputTextModule,
+    InputTextareaModule,
+    ButtonModule,
+    CommonModule
+  ],
     declarations: [ItemComponent],
     bootstrap: [],
     exports: [
